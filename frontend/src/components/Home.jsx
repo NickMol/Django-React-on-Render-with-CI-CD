0e81eab17fcefd69918e9c09b0107c5e02f1552f
CBI Analytics <info@cbi-analytics.nl>
2026-03-02 15:05:17 +0100

    changed homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -21,6 +21,7 @@ const Home = () =>{
 
     return(
         <div> 
+            <div>Homepage</div>
             { loading ? <p>Loading data...</p> :
             <div> 
                 {myData.map((item, index) => (
